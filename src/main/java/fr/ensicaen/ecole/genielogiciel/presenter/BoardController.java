7fd4f78d1cb340de037e8fc4bacc2d2759268331
Lucas OLLIER <lucas.ollier@ecole.ensicaen.fr>
2023-10-03 10:00:23 +0200

    Added 4 pawn in board (with win message)

--- src/main/java/fr/ensicaen/ecole/genielogiciel/presenter/BoardController.java
+++ src/main/java/fr/ensicaen/ecole/genielogiciel/presenter/BoardController.java
@@ -4,6 +4,8 @@ import fr.ensicaen.ecole.genielogiciel.model.Model;
 import fr.ensicaen.ecole.genielogiciel.model.RandomDice;
 import fr.ensicaen.ecole.genielogiciel.view.GameView;
 import fr.ensicaen.ecole.genielogiciel.view.PawnView;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -16,6 +18,7 @@ public final class BoardController {
     private final DicePresenter _dice;
     private int _nbPlayers;
     private int _playerTurn;
+    private int[] _playerPos;
 
     public BoardController( String nickName ) {
         _model = new Model();
@@ -30,21 +33,37 @@ public final class BoardController {
 
     public void create_pawn(int nbPlayers) {
         _nbPlayers = nbPlayers;
+        _playerPos = new int[nbPlayers];
+
         Point[] coordonee = {new Point(40, 40), new Point(60, 40), new Point(40, 60), new Point(60, 60)};
         Color[] color = {Color.RED, Color.BLUE, Color.GREEN, Color.PURPLE};
 
         _pawn = new Rectangle[nbPlayers];
         for (int i = 0; i < nbPlayers; i++) {
-            System.out.println(coordonee[i].x + " " + coordonee[i].y);
             _pawn[i] = PawnView.create(coordonee[i].x, coordonee[i].y, color[i]);
             _view.getBoard().getChildren().add(_pawn[i]);
         }
     }
 
     public void play() {        // public void play(Player player)
-        int nb = _dice.roll();
-        _pawn[_playerTurn].setX(_pawn[_playerTurn].getX() + 100 * nb);
+        int pos = _playerPos[_playerTurn] + _dice.roll();
+        double newX = _pawn[_playerTurn].getX() + 100 * pos;
+        if(pos > 6) {
+            pos = 6 - (pos - 6);
+        }
+        _pawn[_playerTurn].setX(_pawn[_playerTurn].getX() + 100 * (pos - _playerPos[_playerTurn]));
 
+        if(pos == 6) {
+            Alert alert = new Alert(Alert.AlertType.NONE, "Player " + (_playerTurn+1) + " win the game.", ButtonType.OK);
+            alert.setTitle("Winner");
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK) {
+                    System.out.println("Pressed OK.");
+                }
+            });
+        }
+
+        _playerPos[_playerTurn] = pos;
         _playerTurn = (_playerTurn + 1) % _nbPlayers;
     }
 
