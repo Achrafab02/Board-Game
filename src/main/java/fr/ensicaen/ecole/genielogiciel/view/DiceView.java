13ba6c151ccaa26dba44ef77f1bb6743edc4f00f
Bretagne <leo.bretagne@ecole.ensicaen.fr>
2023-10-06 17:26:46 +0200

    Refactored DicePresenter, DiceView & BoardController so that DicePresenter doesn't need javaFX

--- src/main/java/fr/ensicaen/ecole/genielogiciel/view/DiceView.java
+++ src/main/java/fr/ensicaen/ecole/genielogiciel/view/DiceView.java
@@ -4,16 +4,19 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 
 public class DiceView {
-    public AnchorPane _diceBoard;
+    private final AnchorPane _diceBoard;
+    private final String IMAGE_LOCATION = "fr/ensicaen/ecole/genielogiciel/view/dice/dice-";
+    private final String IMAGE_TYPE = ".png";
 
     public DiceView(AnchorPane diceBoard) {
         _diceBoard = diceBoard;
     }
 
-
     public void display(int nb) {
-        _diceBoard.setBackground(new Background(new BackgroundImage(new Image("fr/ensicaen/ecole/genielogiciel/view/dice/dice-" + nb + ".png"),
-                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
-                new BackgroundSize(86, 86, false, false, false, false))));
+        Image image = new Image(IMAGE_LOCATION + nb + IMAGE_TYPE);
+        BackgroundSize backgroundSize = new BackgroundSize(86, 86, false, false, false, false);
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+        _diceBoard.setBackground(background);
     }
 }
