1487b4c90fa525130922f4ca49ce8ac33624854a
Bretagne <leo.bretagne@ecole.ensicaen.fr>
2023-10-06 14:28:08 +0200

    Upgraded Dabbler (magic number + explicit cast)

--- src/main/java/fr/ensicaen/ecole/genielogiciel/model/Brilliant.java
+++ src/main/java/fr/ensicaen/ecole/genielogiciel/model/Brilliant.java
@@ -1,9 +1,9 @@
 package fr.ensicaen.ecole.genielogiciel.model;
 
 public class Brilliant implements SoftSkill {
-    private final float FACTOR = 2;
+    private final double DICE_MODIFIER_FACTOR = 2;
     @Override
     public int modifyDiceRoll(int roll) {
-        return (int) (roll * FACTOR);
+        return (int) (roll * DICE_MODIFIER_FACTOR);
     }
 }
