6004a3f0f425c6081749e24332862bc07d6cc7fe
Bretagne <leo.bretagne@ecole.ensicaen.fr>
2023-10-06 14:23:34 +0200

    Upgraded Brilliant (magic number + explicit cast)

--- src/main/java/fr/ensicaen/ecole/genielogiciel/model/Brilliant.java
+++ src/main/java/fr/ensicaen/ecole/genielogiciel/model/Brilliant.java
@@ -1,8 +1,9 @@
 package fr.ensicaen.ecole.genielogiciel.model;
 
 public class Brilliant implements SoftSkill {
+    private final float FACTOR = 2;
     @Override
     public int modifyDiceRoll(int roll) {
-        return roll * 2;
+        return (int) (roll * FACTOR);
     }
 }
