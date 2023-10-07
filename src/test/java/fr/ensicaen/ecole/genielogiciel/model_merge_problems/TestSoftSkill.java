dce5141dd77d0c928af4e8c44cb3ea6b0cf3efff
Clément CARTRO <clement.cartro@ecole.ensicaen.fr>
2023-10-07 15:08:14 +0200

    Put this file in the same package as the others

--- src/test/java/fr/ensicaen/ecole/genielogiciel/model_merge_problems/TestSoftSkill.java
+++ src/test/java/fr/ensicaen/ecole/genielogiciel/model_merge_problems/TestSoftSkill.java
@@ -1,4 +1,4 @@
-package fr.ensicaen.ecole.genielogiciel.model;
+package fr.ensicaen.ecole.genielogiciel.model_merge_problems;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
