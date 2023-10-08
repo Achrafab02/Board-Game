5eb65447e54a800ba082ace853873210ceafb2d2
Guillain <guillain.le-goff@ecole.ensicaen.fr>
2023-10-08 20:53:22 +0200

    Removed some duplicates test files

--- src/main/java/fr/ensicaen/ecole/genielogiciel/model/board/Pawn.java
+++
@@ -1,9 +0,0 @@
-package fr.ensicaen.ecole.genielogiciel.model.board;
-
-public class Pawn {
-    private static final int MOVE_X = 110;
-
-    public int calculateMove(int numberOfTile) {
-        return numberOfTile * MOVE_X;
-    }
-}
