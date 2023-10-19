5cefcecdec601f0f6433ed3db644d30a04662716
Régis Clouard <regis.clouard@ensicaen.fr>
2023-10-19 09:09:27 +0200

    Reviewed code of the final version

--- src/main/java/fr/ensicaen/ecole/genielogiciel/model/Point.java
+++ src/main/java/fr/ensicaen/ecole/genielogiciel/model/Point.java
@@ -1,6 +1,6 @@
 package fr.ensicaen.ecole.genielogiciel.model;
 
-public class Point {
+public class Point { // FIXME dans le rapport, il est écrit qu'il s'agit d'un Messager ... mais en fait non (décalage entre docuentation et code !)
     private final int _x;
     private final int _y;
 
